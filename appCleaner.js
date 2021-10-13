1373db22b6e373e6e617f8a84ffa1faa694e4538
CAZPFITL <cazpfitl@gmail.com>
2021-10-13 13:15:32 -0500

    feat: cleaner UI improved

--- appCleaner.js
+++ appCleaner.js
@@ -1,7 +1,13 @@
+let r, g, b, W, H, colorByLabel, uid_bycount, users, DownloadData, labelX
 var appFire, dbFire, dbReference, errData, data, keys
-const scale = 20
-const W = 1000
-const H = 500
+/**
+ * Configuration size
+ */
+labelX = 'red-ish'
+const scale = 50
+/**
+ * Helpers
+ */
 const firebaseConfig = {
     apiKey: "AIzaSyALJK23uLX46X1wNR_lDJzdLEahoW7IEtU",
     authDomain: "color-data-a1f4c.firebaseapp.com",
@@ -11,71 +17,44 @@ const firebaseConfig = {
     appId: "1:930703974581:web:bc1046424d31729f135efc",
     measurementId: "G-1RGVF8Y3ND"
 };
-let colorByLabel = {
-    'red-ish': [],
-    'green-ish': [],
-    'blue-ish': [],
-    'orange-ish': [],
-    'yellow-ish': [],
-    'pink-ish': [],
-    'purple-ish': [],
-    'brown-ish': [],
-    'grey-ish': [],
-}
-let DownloadData = {
-    entries: []
-}
-let labelX = 'red-ish'
-let uid_bycount = {}
-let users = []
-let r, g, b
-
-function setup() {
-    createCanvas(W, H)
-    loadDB(firebaseConfig)
-}
-
-function loadDB() {
-    //Initialize Firebase
-    appFire = appFire ?? firebase.initializeApp(firebaseConfig)
-    dbFire = dbFire ?? firebase.database()
-    dbReference = dbReference ?? dbFire.ref('tone')
-    dbReference.once('value', gotData, errData)
-}
-
-function mousePressed() {
-    let i = floor(mouseX / scale)
-    let j = floor(mouseY / scale)
-    let index = i + j * (width / scale)
-    let data = colorByLabel[labelX]
-
-    if (mouseX < W && mouseY < H && data[index]) {
-        console.log(data[index])
-        if (confirm('Are you sure you want to delete this record from the database?')) {
-            let elementToRemove = firebase.database().ref(`tone/${data[index].key}`)
-
-            elementToRemove.remove()
-                .then(function () {
-                    console.log('wrong color deleted from the database.');
-                })
-                .catch(function (error) {
-                    console.log("Remove failed: " + error.message)
-                });
-
-        } else {
-            console.log('ok.');
-        }
-        thisHouseIsCleanNow()
-    }
-}
-
-function thisHouseIsCleanNow() {
-    restartPushedData() // clear data 
-    clear() // Clear canvas & html
+const thisHouseIsCleanNow = () => {
+    initializeDataSet() // clear data 
     loadDB() // Load Data Again
 }
+const drawSelect = () => {
+    sel = createSelect();
+    sel.position(8, (H));
+    sel.option('red-ish');
+    sel.option('green-ish');
+    sel.option('blue-ish');
+    sel.option('orange-ish');
+    sel.option('yellow-ish');
+    sel.option('pink-ish');
+    sel.option('purple-ish');
+    sel.option('brown-ish');
+    sel.option('grey-ish');
+    sel.selected(labelX);
+    sel.changed((e) => { labelX = e.target.value; thisHouseIsCleanNow(); })
+}
+const getButton = () => {
+    button = createButton('Download Data')
+    button.position(120, H)
+    button.mouseReleased(() => saveJSON(DownloadData, 'colorData.json'))
 
-function restartPushedData() {
+}
+const getSizes = () => {
+    let colorSet = colorByLabel[labelX]
+    
+    let Width = ((window.innerWidth - scale) - window.innerWidth.toString().slice(-1))
+    Width = Math.trunc(Width / scale) * scale
+
+    let Height = (colorSet.length - colorSet.length.toString().slice(-1)) / (Width / scale)
+    Height = (round(Height)) * scale
+    
+    return [Width, Height]
+}
+
+const initializeDataSet = () => {
     colorByLabel = {
         'red-ish': [],
         'green-ish': [],
@@ -89,6 +68,31 @@ function restartPushedData() {
     }
     uid_bycount = {}
     users = []
+    DownloadData = {
+        entries: []
+    }
+}
+
+const loadDB = () => {
+    //Initialize Firebase
+    appFire = appFire ?? firebase.initializeApp(firebaseConfig)
+    dbFire = dbFire ?? firebase.database()
+    dbReference = dbReference ?? dbFire.ref('tone')
+    dbReference.once('value', gotData, errData)
+}
+
+const resizeMyCanvas = () => {
+    let sizes = getSizes()
+    let W = sizes[0]
+    let H = sizes[1]
+    resizeCanvas(W, H)
+}
+
+function setup() {
+    noCanvas()
+    createCanvas(window.innerWidth * .98, window.innerHeight)
+    noStroke()
+    thisHouseIsCleanNow()
 }
 
 function gotData(response) {
@@ -109,10 +113,37 @@ function gotData(response) {
             uid_bycount[id]++
         }
     }
+    clear() //clear canvas
+    resizeMyCanvas()
     drawData()
     writeData(keys)
     drawSelect()
     getButton()
+}
+
+function mousePressed() {
+    let sizes = getSizes()
+    let i = floor(mouseX / scale)
+    let j = floor(mouseY / scale)
+    let index = i + j * (sizes[0] / scale)
+    let data = colorByLabel[labelX]
+    if (mouseX < sizes[0] && mouseY < sizes[1] && data[index]) {
+        if (confirm('Are you sure you want to delete this record from the database?')) {
+            let elementToRemove = firebase.database().ref(`tone/${data[index].key}`)
+
+            elementToRemove.remove()
+                .then(function () {
+                    console.log('"wrong color" deleted from the database.');
+                })
+                .catch(function (error) {
+                    console.log("Remove failed: " + error.message)
+                });
+
+        } else {
+            console.log('ok.');
+        }
+        thisHouseIsCleanNow()
+    }
 }
 
 function drawData() {
@@ -124,6 +155,7 @@ function drawData() {
         fill(colorSet[index].r, colorSet[index].g, colorSet[index].b)
         rect(x, y, scale, scale)
         x += scale
+        console.log(width)
         if (x >= width) {
             x = 0
             y += scale
@@ -138,7 +170,7 @@ function writeData(keys) {
             let outTag = document.createElement('div')
             outTag.id = 'data'
             outTag.innerHTML = htmlOutput
-            document.querySelector('body').append(outTag)
+            document.querySelector('main').append(outTag)
         } else {
             dom.innerHTML = htmlOutput
         }
@@ -154,27 +186,4 @@ function writeData(keys) {
 
     htmlOutput += '</div>'
     drawOutter(htmlOutput)
-}
-
-function drawSelect() {
-    sel = createSelect();
-    sel.position(0, (H));
-    sel.option('red-ish');
-    sel.option('green-ish');
-    sel.option('blue-ish');
-    sel.option('orange-ish');
-    sel.option('yellow-ish');
-    sel.option('pink-ish');
-    sel.option('purple-ish');
-    sel.option('brown-ish');
-    sel.option('grey-ish');
-    sel.selected(labelX);
-    sel.changed((e) => { labelX = e.target.value; thisHouseIsCleanNow(); })
-}
-
-function getButton() {
-    button = createButton('Download Data')
-    button.position(120, H)
-    button.mouseReleased(()=>saveJSON(DownloadData, 'colorData.json'))
-
 }
