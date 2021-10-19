ef43f21490c9c733e01984586d74aeb28269a065
CAZPFITL <cazpfitl@gmail.com>
2021-10-19 02:02:19 -0500

    Update appClassifier.js

--- appClassifier.js
+++ appClassifier.js
@@ -63,7 +63,7 @@ const handleFile = (file) => {
     process()
 }
 
-const loadModel = async () => model = await tf.loadLayersModel('my-model.json')
+const loadModel = async () => model = await tf.loadLayersModel('/atrainer/my-model.json')
 
 const process = () => {
     createTensor();
